85675df4c9e9cdff14292f8543484763f216cee6
Patrick Grimard <pgrimard@gmail.com>
2016-03-23 13:56:07 -0400

    Disabled client discovery

--- src/main/java/com/patrickgrimard/Application.java
+++ src/main/java/com/patrickgrimard/Application.java
@@ -2,10 +2,9 @@ package com.patrickgrimard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
