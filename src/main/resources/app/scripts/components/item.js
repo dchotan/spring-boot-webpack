26a5f1cd16972467c46cee3390263de62cad4c00
Patrick Grimard <pgrimard@gmail.com>
2016-10-27 12:28:21 -0400

    Deleted obsolete item.js and itemlist.js

--- src/main/resources/app/scripts/components/item.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import 'styles/components/item.scss';
-
-export default class Item extends React.Component {
-  render() {
-    return <li className="item">{this.props.value}</li>;
-  }
-}
