69be1a6b40e54d17b258b0cfd7c07b5995e0796c
Patrick Grimard <pgrimard@gmail.com>
2016-10-27 12:26:36 -0400

    Moved Application to src/main/kotlin

--- src/main/kotlin/com/patrickgrimard/Application.kt
+++ src/main/kotlin/com/patrickgrimard/Application.kt
@@ -2,23 +2,9 @@ package com.patrickgrimard
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Bean
 
-/**
- *
- *
- * Created on 2016-03-24
- *
- * @author Patrick
- */
 @SpringBootApplication
-open class Application {
-
-    @Bean
-    open fun messageService() : MessageService {
-        return MessageServiceImpl()
-    }
-}
+open class Application
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
