19c4b19d00df20625a9a92beb8d343091391d841
Patrick Grimard <pgrimard@gmail.com>
2016-10-27 12:28:30 -0400

    Added Main.js

---
+++ src/main/resources/app/scripts/components/Main.js
@@ -0,0 +1,34 @@
+import React, {PropTypes, Component} from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {fetchMessages} from 'components/message/MessageActionCreators';
+import MessageList from 'components/message/MessageList';
+import 'styles/main.scss';
+
+const propTypes = {
+  fetchItems: PropTypes.func
+};
+
+class Main extends Component {
+  componentDidMount() {
+    const {fetchMessages} = this.props;
+    fetchMessages();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Spring Boot Rocks!</h1>
+        <MessageList/>
+      </div>
+    );
+  }
+}
+
+Main.propTypes = propTypes;
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchMessages}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(Main);
